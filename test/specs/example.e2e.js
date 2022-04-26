54838f03984a4b00df7a7ceca5342660a4c5077f
Ashok Reddy <ashokcse505@gmail.com>
2022-04-26 15:28:10 +0530

    added console

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -9,6 +9,7 @@ describe('My Login application', () => {
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
+            console.log("hello my app")
     });
 });
 
